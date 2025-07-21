d50435852b64995a4a403a5e5d262f00e47e60f6
tjakra <cakrasatria01@gmail.com>
2025-07-22 00:22:44 +0700

    add rotation screen for mobile

--- js/function.js
+++ js/function.js
@@ -6,24 +6,12 @@ function frame(w,h){
 function screen(){
     const elem = document.getElementById("area");
     // 1. Masuk fullscreen
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen().then(() => {
-        // 2. Rotasi jika mobile
-        if (isMobile()) {
-            rotateToLandscape();
-        }
-        });
+    elem.requestFullscreen()
+    if(isMobile()){
+      myscreen.lock("landscape")
     }
 }
 
 function isMobile() {
   return /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
-}
-
-function rotateToLandscape() {
-  if (screen.orientation && screen.orientation.lock) {
-    screen.orientation.lock("landscape").catch(err => {
-      console.warn("Rotasi gagal:", err);
-    });
-  }
 }
--- js/var_global.js
+++ js/var_global.js
@@ -1,2 +1,3 @@
 const canva = document.querySelector("canvas")
 const ctx = canva.getContext('2d')
+var myscreen = window.screen.orientation
